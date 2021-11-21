fa062cf98bd4b0fef74abbadf971f92ed6439b6d
Josh Polansky <josh@loupe.team>
2021-11-21 11:29:48 -0800

    Remove dependencies
    they will be included in the headers if
    the dependent libraries are in the project.

--- omjsonInternal.h
+++ omjsonInternal.h
@@ -2,12 +2,3 @@
 #include "bur.h"
 #define __asm__(...)
 #endif
-
-#include "AsHttp.h"
-#include "VarTools.h"
-#include "DataBuffer.h"
-#include "AsTCP.h"
-#include "TCPComm.h"
-#include "AsGuard.h"
-#include "standard.h"
-
